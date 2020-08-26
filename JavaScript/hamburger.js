fbe3805aac13ade49b67fdef7f6b4ecaa1d090e4
Shosei Takayama <itsumen.syosei@icloud.com>
2020-08-26 15:10:53 +0900

    ハンバーガーメニュー実装完了

--- JavaScript/hamburger.js
+++ JavaScript/hamburger.js
@@ -1,11 +1,29 @@
 jQuery (function($){
 
-    $(".is-hamburger").on("click", function() {
+    $(".p-hamburger").on("click", function() {
 
-      $( this ).toggleClass( "open" );
-      $( "nav" ).toggleClass( "open" );
-      $( "body" ).toggleClass( "open" );
+      if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        $('body').removeClass('open');
+        $('nav').removeClass('open');
+        $('.l-sidebar-overlay').removeClass('open');
+      } else {
+        $(this).addClass('active');
+        $('body').addClass('open');
+        $('nav').addClass('open');
+        $('.l-sidebar-overlay').addClass('open');
+      }
     ;}
-    );
-  });
 
+    );
+
+    $('.l-sidebar-overlay').on('click',function(){
+      if($(this).hasClass('open')){
+        $(this).removeClass('open');
+        $('.menu-trigger').removeClass('active');
+        $('main').removeClass('open');
+        $('nav').removeClass('open');      
+      }
+    });
+  
+  });
