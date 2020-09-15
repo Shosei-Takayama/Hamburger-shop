da915bc5b91633b6bd5cc472ff1e36c858ab155c
Shosei Takayama <itsumen.syosei@icloud.com>
2020-09-15 14:26:43 +0900

    各コンテンツのimport 化完了

--- JavaScript/hamburger.js
+++ JavaScript/hamburger.js
@@ -2,28 +2,36 @@ jQuery (function($){
 
     $(".p-hamburger").on("click", function() {
 
+      $( this ).toggleClass( "open" );
+      $( "nav" ).toggleClass( "open" );
+      $( "body" ).toggleClass( "open" );
+      
+    ;}
+    );
+  });
+
+
+  jQuery (function($){
+
+    $(".p-hamburger").on("click", function() {
+
       if($(this).hasClass('active')){
         $(this).removeClass('active');
         $('body').removeClass('open');
         $('nav').removeClass('open');
+        $('p-hamburger').removeClass('open');
         $('.l-sidebar-overlay').removeClass('open');
       } else {
         $(this).addClass('active');
         $('body').addClass('open');
         $('nav').addClass('open');
+        $('p-hamburger').addClass('open');
         $('.l-sidebar-overlay').addClass('open');
       }
     ;}
 
     );
 
-    $('.l-sidebar-overlay').on('click',function(){
-      if($(this).hasClass('open')){
-        $(this).removeClass('open');
-        $('.menu-trigger').removeClass('active');
-        $('main').removeClass('open');
-        $('nav').removeClass('open');      
-      }
-    });
+    
   
   });
